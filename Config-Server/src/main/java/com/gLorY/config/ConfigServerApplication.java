adae6bc05400d1566d4fa3a6ca4fce7f4517c53b
fengsewwq <fengsewwq@163.com>
2017-08-17 15:57:42 +0800

    wwq

---
+++ Config-Server/src/main/java/com/gLorY/config/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.gLorY.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
+
+/*http请求地址和资源文件映射如下:
+
+/{application}/{profile}[/{label}]
+/{application}-{profile}.yml
+/{label}/{application}-{profile}.yml
+/{application}-{profile}.properties
+/{label}/{application}-{profile}.properties*/
